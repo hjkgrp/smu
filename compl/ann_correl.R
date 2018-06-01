80c3f890854b6a78156e2a445c157440b6510146
sgugler <sgugler@mit.edu>
2018-05-31 20:27:33 -0400

    almost finished the GM1 presentation

--- compl/ann_correl.R
+++ compl/ann_correl.R
@@ -1,24 +1,6 @@
-df <- as.data.frame(homolepticRF41KRRspins$krrsplit[(0*405+1):(1*405)])
-df$e1 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(0*405+1):(1*405)])
-df$e2 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(1*405+1):(2*405)])
-df$e3 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(2*405+1):(3*405)])
-df$e4 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(3*405+1):(4*405)])
-df$e5 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(4*405+1):(5*405)])
-df$e6 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(5*405+1):(6*405)])
-df$e7 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(6*405+1):(7*405)])
-df$e8 <- as.data.frame(homolepticRF41KRRspins$krrsplit[(7*405+1):(8*405)])
+df <- as.data.frame(homolepticRF41KRRSpins)
 
-colnames(df)[1] <- 'e1_res'
-colnames(df)[2] <- 'e1'
-colnames(df)[3] <- 'e2'
-colnames(df)[4] <- 'e3'
-colnames(df)[5] <- 'e4'
-colnames(df)[6] <- 'e5'
-colnames(df)[7] <- 'e6'
-colnames(df)[8] <- 'e7'
-colnames(df)[9] <- 'e8'
 
-df$idu <- as.numeric(row.names(df))
 
 g <- ggplot(data=df, aes(x=idu, y=e1, color='red'), size = 1) + geom_point()
 g <- g + geom_point(data=df, aes(y=e2, color='blue'))
--- compl/process.r
+++ compl/process.r
@@ -4,7 +4,7 @@ readResults <- function(path, charge){
   results <- read.csv(file = path)
   
   # denote the charged ligands with 1 the neutral ones with 0 and combine them
-  results$charge <- charge #rep(0,nrow(results1)) 
+  # results$charge <- charge #rep(0,nrow(results1)) 
 }
 
 # name rows and cols (ie but them into rownames) and rm the actual col
@@ -19,16 +19,23 @@ racs1 <- readRacs("consistent_descriptor_file.csv")
 racs2 <- readRacs("consistent_descriptor_file_charged.csv")
 racs3 <- readRacs("consistent_descriptor_file_bi.csv")
 racs4 <- readRacs('consistent_descriptor_file_charged_bi.csv')
-racsNan <- readRacs('consistent_descriptor_file_nans.csv')
+racsNan1 <- readRacs('consistent_descriptor_file_nans.csv')
+racsNan2 <- readRacs('consistent_descriptor_file_charged_nans.csv')
 
 results1 <- readResults('unified_results_post.csv', 0)
 results2 <- readResults('unified_results_post_charged.csv', 1)
 results3 <- readResults('unified_results_post_bi.csv', 0)
 results4 <- readResults('unified_results_post_charged_bi.csv', 1)
-resultsNan <- readResults('unified_results_post_nans.csv', 0)
+resultsNan1 <- readResults('unified_results_post_nans.csv', 0)
+resultsNan2 <- readResults('unified_results_post_charged_nans.csv', 1)
 
-racs <- rbind(rbind(rbind(rbind(racs1, racsNan), racs2), racs3), racs4)
-results <- rbind(rbind(rbind(rbind(results1, resultsNan), results2), results3), results4)
+# because the charged ones have 5 more cols because they were produced with a slightly earlier version of mAD
+results1 <- results1[,(colnames(results1) %in% colnames(results2))]
+results3 <- results3[,(colnames(results3) %in% colnames(results4))]
+resultsNan1 <- resultsNan1[,(colnames(resultsNan1) %in% colnames(resultsNan2))]
+
+racs <- rbind(rbind(rbind(rbind(rbind(racs1, racsNan1), racs2), racsNan2), racs3), racs4)
+results <- rbind(rbind(rbind(rbind(rbind(results1, resultsNan1), results2), resultsNan2), results3), results4)
 
 # generate all columns that are interesting to us
 colstokeep <- c("name","gene","alpha","metal","ox2RN","ox3RN","ox_2_split","ox_3_split")
@@ -75,6 +82,7 @@ split_results <- rbind(ox_2_split_results,ox_3_split_results)
 require(plyr)
 split_results$metal <- revalue(as.factor(split_results$metal),c("0"="Cr","1"="Mn","2"="Fe","3"="Co"))
 split_results$ox <- as.factor(split_results$ox)
+split_results$run <-
 
 # require(ggplot2)
 # gg<-ggplot(data=split_results,aes(x=V104,y=split,color=ox,group=ox)) +geom_point() + facet_grid("metal ~.") + theme_light()
